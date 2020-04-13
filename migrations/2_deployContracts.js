dcadbc5b4186b0777b9ce52b0d10d45c0f55df28
rachit2501 <rac.sri25@gmail.com>
2020-04-13 13:10:11 +0530

    one test failing

--- migrations/2_deployContracts.js
+++ migrations/2_deployContracts.js
@@ -1,5 +1,9 @@
 const Token = artifacts.require("Token");
+const TokenSale = artifacts.require('TokenSale');
+var tokenprice = 100000000000000;
 
-module.exports = function(deployer) {
-  deployer.deploy(Token , 1000000);
+module.exports = async function(deployer) {
+  await deployer.deploy(Token , 1000000);
+  await deployer.deploy(TokenSale , Token.address , tokenprice);
+
 };
